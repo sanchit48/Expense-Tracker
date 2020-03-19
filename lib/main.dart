c9f3a2c5497ffcf920df94202b3170adc0228f8d
sanchit48 <sanchitkhare3@gmail.com>
2020-03-19 14:52:57 +0530

    Adding adaptive design code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:expense_tracker/widgets/transaction_list.dart';
-import './widgets/new_transaction.dart';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
 import './widgets/chart.dart';
 import './models/transaction.dart';
-import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -72,22 +75,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransaction {
 
     return _userTransaction.where((tx) {
+
       return tx.date.isAfter(
+
         DateTime.now().subtract(
+
           Duration(days:7)
+
         ),
+
       );
+
     }).toList();
+
   }
 
   void _addNewTransaction(String title, double amount, DateTime chosenDate) {
 
     setState(() {
+
         _userTransaction.add(Transaction(id: DateTime.now().toString(), title: title, amount: amount, date: chosenDate));
     });
+
   }
 
   void _deleteTransaction(String id) {
@@ -109,36 +123,68 @@ class _MyHomePageState extends State<MyHomePage> {
     showModalBottomSheet(context: ctx, builder: (_) {
 
       return GestureDetector(
+
         onTap: () {},
         child: NewTransaction(_addNewTransaction),
         behavior: HitTestBehavior.opaque,
+
       );
 
     },);
+
   }
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
+    final mediqQuery = MediaQuery.of(context);
 
-      appBar: AppBar(
+    final isLandScape = (mediqQuery.orientation == Orientation.landscape);
 
-        title: Text('Personal Expenses'),
-        actions: <Widget>[
+    final PreferredSizeWidget appBar =  Platform.isIOS
+    ? CupertinoNavigationBar(
 
-          IconButton(
+      middle: Text('Personal Expenses'),
+      trailing: Row(
 
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
+        mainAxisSize: MainAxisSize.min,
+        children: [
+        // make own button
+        GestureDetector(
 
-          )
+          child: Icon(CupertinoIcons.add),
+          onTap: () => _startAddNewTransaction(context),)
 
-        ],
+      ]),
 
-      ),
+    )
 
-      body: SingleChildScrollView(
+    : AppBar(
+
+      title: Text('Personal Expenses'),
+      actions: <Widget>[
+
+        IconButton(
+
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+
+        )
+
+      ],
+
+    );
+
+    final txListWidget  = Container(
+
+        height: (mediqQuery.size.height - appBar.preferredSize.height - mediqQuery.padding.top),
+        child: TransactionList(_userTransaction, _deleteTransaction)
+
+    );
+
+    final pageBody = SafeArea(
+
+      child: SingleChildScrollView(
 
         child: Column(
 
@@ -146,18 +192,71 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
 
-            Chart(_recentTransaction),
+              if(isLandScape) Row(
 
-            TransactionList(_userTransaction, _deleteTransaction),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                Text('Show Chart', style: Theme.of(context).textTheme.title,),
+                Switch.adaptive(
+
+                  activeColor: Theme.of(context).accentColor,
+                  value: _showChart,
+                  onChanged: (value) {
+
+                    setState(() {
+                      _showChart = value;
+                    });
+
+                  },
+
+                ),
+
+              ],
+
+            ),
+
+            if(!isLandScape) Container(
+
+              height: (mediqQuery.size.height - appBar.preferredSize.height - mediqQuery.padding.top)*0.3,
+              child: Chart(_recentTransaction)
+
+            ),
+
+            if(!isLandScape) txListWidget,
+
+            if(isLandScape)_showChart ? Container(
+
+              height: (mediqQuery.size.height - appBar.preferredSize.height - mediqQuery.padding.top)*.7,
+              child: Chart(_recentTransaction)
+
+            ) : txListWidget
 
           ],
 
         ),
 
-      ),
+      ));
+
+    return Platform.isIOS
+    ? CupertinoPageScaffold(
+
+      child: pageBody,
+      navigationBar: appBar,
+
+    )
+
+    : Scaffold(
+
+      appBar: appBar,
+      body: pageBody,
+
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: Platform.isIOS
+        ? ''
+
+        : FloatingActionButton(
 
         child: Icon(Icons.add),
         onPressed: () => _startAddNewTransaction(context),
